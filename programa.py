f6877e303c42416a88e2482844553248369fd93c
karolyuuki <karoline-nevesb@hotmail.com>
2015-12-22 11:55:25 +0900

    Update programa.py

--- programa.py
+++ programa.py
@@ -1,25 +1,31 @@
 import funcoes
 
-c1, c2, c3, cent1, cent2, cent3 = funcoes.centroide('normals.txt')
+centroids = list()
+medias = list()
+for it in range(0,10):
 
-print(c1)
-print(c2)
-print(c3)
-a = 0
-b = 0
-c = 0
+    c1, c2, c3, cent1, cent2, cent3 = funcoes.centroide('normals.txt')
 
-for i in cent1:
-    a= a + funcoes.distanciaeuclidiana(c1,i)
-med1= a/len(cent1)
-print (med1)
+    centroids.append([c1,c2,c3])
+    a = 0
+    b = 0
+    c = 0
 
-for i in cent2:
-    b= b + funcoes.distanciaeuclidiana(c2,i)
-med2= b/len(cent2)
-print (med2)
+    for i in cent1:
+        a= a + funcoes.distanciaeuclidiana(c1,i)
+    med1= a/len(cent1)
 
-for i in cent3:
-    c= c + funcoes.distanciaeuclidiana(c3,i)
-med3= c/len(cent3)
-print (med3)
+    for i in cent2:
+        b= b + funcoes.distanciaeuclidiana(c2,i)
+    med2= b/len(cent2)
+
+    for i in cent3:
+        c= c + funcoes.distanciaeuclidiana(c3,i)
+    med3= c/len(cent3)
+
+    media = (med1+med2+med3)/3
+    medias.append([med1,med2,med3,media])
+    
+
+print(len (centroids))
+print(len (medias))
