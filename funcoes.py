c4abc72e5bd7abb60c2d0baebdc04b7108314cc1
karolyuuki <karoline-nevesb@hotmail.com>
2016-01-05 14:29:04 +0900

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,5 +1,5 @@
 import math
-
+import random
 
 def distanciaeuclidiana(i,j):
     n = len(i)
@@ -14,7 +14,6 @@ def separadados(filename):
     filex = open (filename, 'r')
     list_items = filex.readlines()
     size =(len(list_items))
-    print (size)
     Llabel = list()
     attr = list()
     for item in list_items:
@@ -74,11 +73,47 @@ def centroide(lista):
         else:
             Cent3.append(nitem)
 
-    print(len(Cent1), len(Cent2), len(Cent3))
+    #print(len(Cent1), len(Cent2), len(Cent3))
 
+    #for i,iname in zip([Cent1, Cent2, Cent3],[1,2,3]):
+        #for j in i:
+            #for k in j:
+                #print (str(k)+",")
+            #print(str(iname)+"\n")
+    return Centroide1, Centroide2, Centroide3, Cent1, Cent2, Cent3
+
+def cluster(lista,centroides):
+    Attrb, labels = funcoes.separadados(lista)
+
+
+    c = len(Attrb)
+    Centro = centroides[1]
+    Centroide1 = Centro[0]
+    Centroide2 = Centro[1]
+    Centroide3 = Centro[2]
+
+
+    Cent1 = list()
+    Cent2 = list()
+    Cent3 = list()
+
+    for item in Attrb:
+        nitem = funcoes.converteitens(item)
+        a = distanciaeuclidiana(nitem,Centroide1)
+        b = distanciaeuclidiana(nitem,Centroide2)
+        c = distanciaeuclidiana(nitem,Centroide3)
+        if a<b and a<c:
+            Cent1.append(nitem)
+        elif b<a and b<c:
+            Cent2.append(nitem)
+        else:
+            Cent3.append(nitem)
+
+    #print(len(Cent1), len(Cent2), len(Cent3))
+    newarchive= open ('lists_cluster.txt','w')
     for i,iname in zip([Cent1, Cent2, Cent3],[1,2,3]):
         for j in i:
             for k in j:
-                print(str(k)+",")
-            print(str(iname)+"\n")
-    return Centroide1, Centroide2, Centroide3, Cent1, Cent2, Cent3
+                newarchive.write(str(k)+",")
+            newarchive.write(str(iname)+"\n")
+    return Cent1, Cent2, Cent3
