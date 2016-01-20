bff671c4cf26de7f0ea089d9835dc46188c6e386
karolyuuki <karoline-nevesb@hotmail.com>
2016-01-20 14:51:25 +0900

    Update newmethod.py

--- newmethod.py
+++ newmethod.py
@@ -36,7 +36,7 @@ def bestCentroid(onelist):
 
         media = (med1+med2+med3)/3
         cont = cont+1
-        print(cont, media)
+        #print(cont, media)
         if (media<= 3.5):
             a = 0
             centroids.append(c1)
@@ -60,38 +60,52 @@ print (Genmedia, medias)
 cents = list()
 cents =funcoes.cluster2(lista,centroides)
 
-def Srandom(medias, centroides):
+def Srandom(medias, centroides, times, mediaoriginal):
     cont = 0
     ncentroides = centroides
-    for i,j in zip(medias,centroides):
+    med,cent1,cent2,cent3,amedias = funcoes.centroide2(lista, centroides)
+    for i in medias:
         if (i > 3.1 and i <=3.5):
+            print(ncentroides[cont])
             aux = list()
             aux = cents[cont]
-            aux = aux[1]
+            if times == ((len(aux))-1):
+                print(times)
+                return (centroides, cent1, cent2, cent3, mediaoriginal)
+            aux = aux[times]
             print(aux)
             ncentroides[cont] = aux[1]
-        if (i > 3.5 and i <=5):
+            print(ncentroides[cont])
+        if (i > 3.5):
             aux = list()
             aux = cents[cont]
             middle = round(len(aux)/2)
-            aux = aux[middle]
+            if times == ((middle-2)):
+                print(times)
+                return (centroides, cent1, cent2, cent3, mediaoriginal)
+            aux = aux[middle+times]
             ncentroides[cont] = aux[1]
+        if (i<=3.1):
+            ncentroides[cont]= ncentroides[cont]
         cont = cont+1
-    return(ncentroides)
-ncentroides = Srandom(medias, centroides)
-nmed,ncet1,ncent2,ncent3 = funcoes.centroide2(lista, ncentroides)
-print(nmed)
-if (Genmedia<nmed):
-    print('serviu de nada essa mudanca ai')
-else:
-    print('melhorou!')
-#usa uma recursividade pra depois que comparar os dois
-#pegar o com a media menor e fazer tudo denovo
+        
+    print('ncentroides')
+    print(ncentroides)
+    nmed,ncent1,ncent2,ncent3,nmedias = funcoes.centroide2(lista, ncentroides)
+    print(nmedias)
+    if ((nmedias[0]<=3.1)and (nmedias[1]<=3.1)and(nmedias[2]<=3.1)):
+        return (ncentroides, ncent1, ncent2, ncent3, nmed)
+    print(nmed)
+    if (mediaoriginal<=nmed):
+        print('serviu de nada essa mudanca ai')
+        times = times+1
+        #print (times)
+        Srandom(medias, centroides, times, mediaoriginal)
+    else:
+        print('melhorou!')
+        novamedia=nmed
+        Srandom(nmedias,ncentroides,1,novamedia)
+    return(ncentroides, ncent1, ncent2, ncent3, nmed)
 
-# If the average distance is between 3.1 and 3.8, we will choose
-# in the list of this centroid what item is closer to them and turn it the
-# new centroid. If the average distance is between 3.8 and 5, we will choose
-# someone in the middle of the list.
-
-
-
+ncentroides, ncent1, cent2, cent3, nmed = Srandom(medias,centroides, 1, Genmedia)
+print (nmed)
