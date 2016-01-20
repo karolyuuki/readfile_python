8413a4e767fa4e0e8f2898c43889b3acc3b0ae9c
karolyuuki <karoline-nevesb@hotmail.com>
2016-01-20 09:37:04 +0900

    Update newmethod.py

--- newmethod.py
+++ newmethod.py
@@ -2,7 +2,6 @@ import funcoes
 
 def bestCentroid(onelist):
     centroids = list()
-    medias = list()
     med = list()
     a=1
     cont=0
@@ -43,7 +42,7 @@ def bestCentroid(onelist):
             centroids.append(c1)
             centroids.append(c2)
             centroids.append(c3)
-            medias.append(media)
+            medias = media
             med.append(med1)
             med.append(med2)
             med.append(med3)
@@ -57,14 +56,42 @@ Genmedia,centroides,medias = bestCentroid(lista)
 print (centroides)
 print (Genmedia, medias)
 
-Cents = list()
-Cents =funcoes.cluster2(lista,centroides)
 
-#for i,j in zip(medias,centroides):
-    #if (i > 3.1 and i <=3.5):
-    
+cents = list()
+cents =funcoes.cluster2(lista,centroides)
+
+def Srandom(medias, centroides):
+    cont = 0
+    ncentroides = centroides
+    for i,j in zip(medias,centroides):
+        if (i > 3.1 and i <=3.5):
+            aux = list()
+            aux = cents[cont]
+            aux = aux[1]
+            print(aux)
+            ncentroides[cont] = aux[1]
+        if (i > 3.5 and i <=5):
+            aux = list()
+            aux = cents[cont]
+            middle = round(len(aux)/2)
+            aux = aux[middle]
+            ncentroides[cont] = aux[1]
+        cont = cont+1
+    return(ncentroides)
+ncentroides = Srandom(medias, centroides)
+nmed,ncet1,ncent2,ncent3 = funcoes.centroide2(lista, ncentroides)
+print(nmed)
+if (Genmedia<nmed):
+    print('serviu de nada essa mudanca ai')
+else:
+    print('melhorou!')
+#usa uma recursividade pra depois que comparar os dois
+#pegar o com a media menor e fazer tudo denovo
 
 # If the average distance is between 3.1 and 3.8, we will choose
 # in the list of this centroid what item is closer to them and turn it the
 # new centroid. If the average distance is between 3.8 and 5, we will choose
 # someone in the middle of the list.
+
+
+
